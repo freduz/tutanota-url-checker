02c642a3e38620b2af523df1f1c3ea10d6bf8978
Fredy Daniel <fredyd900@gmail.com>
2023-06-02 14:25:22 +0200

    refactoring-solution

--- dist/index.js
+++ dist/index.js
@@ -12,9 +12,10 @@ const urlInput = document.getElementById('url');
 const alertBox = document.getElementById("alert-box");
 const statusblock = document.getElementById("status");
 const urlInfoBlock = document.getElementById('url-info-card');
+const checkResult = document.getElementById("check-status");
 const PATTERN = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/;
 urlInput.addEventListener('input', processURL);
-const thorottleURL = handleRequest(checkExistence, 3000);
+const thorottleURL = debounceAndThrottle(checkExistence, 300, 1000);
 function processURL() {
     return __awaiter(this, void 0, void 0, function* () {
         if (!URLChecker(this.value)) {
@@ -25,7 +26,7 @@ function processURL() {
         }
         alertBox.style.display = 'none';
         try {
-            yield thorottleURL(this.value);
+            thorottleURL(this.value);
         }
         catch (error) {
         }
@@ -34,48 +35,43 @@ function processURL() {
 const URLChecker = (url) => {
     return PATTERN.test(url);
 };
-function handleRequest(func, limit) {
-    let inThrottle = false;
-    return function (url) {
-        return __awaiter(this, arguments, void 0, function* () {
-            const args = arguments;
-            const context = url;
-            if (!inThrottle) {
-                yield func(context);
-                inThrottle = true;
-                setTimeout(() => (inThrottle = false), limit);
-            }
-        });
+function debounceAndThrottle(func, delay, limit) {
+    let timerId;
+    let lastCallTime = 0;
+    let isThrottled = false;
+    return (...args) => {
+        const currentTime = Date.now();
+        clearTimeout(timerId);
+        if (currentTime - lastCallTime >= limit) {
+            func(args);
+            lastCallTime = currentTime;
+        }
+        else {
+            timerId = setTimeout(() => {
+                func(args);
+                lastCallTime = currentTime;
+            }, delay);
+        }
     };
 }
 function checkExistence(url) {
     return __awaiter(this, void 0, void 0, function* () {
-        yield new Promise((resolve) => setTimeout(resolve, 1000));
-        let message = '';
-        let heading = '';
-        const exists = Math.random() < 0.5;
-        if (exists) {
-            message = "The entered URL found on the server";
-            heading = "Healthy URL";
-            urlInfoBlock.children[0].children[1].classList.add('bg-teal-100');
-            urlInfoBlock.children[0].children[1].classList.add('border-teal-500');
-            urlInfoBlock.children[0].children[1].classList.remove('bg-red-100');
-            urlInfoBlock.children[0].children[1].classList.remove('border-red-500');
-            urlInfoBlock.children[0].children[1].children[0].children[0].children[0].classList.remove('text-red-500');
-            urlInfoBlock.children[0].children[1].children[0].children[0].children[0].classList.add('text-teal-500');
+        try {
+            const status = yield checkingServer(url);
+            const result = status.exists ? (status.isFile ? 'File exists' : 'Folder exists') : 'URL does not exist';
+            urlInfoBlock.style.display = "flex";
+            checkResult.textContent = result;
         }
-        else {
-            message = "Sorry! the entred URL is not found";
-            heading = "BAD URL";
-            urlInfoBlock.children[0].children[1].classList.remove('bg-teal-100');
-            urlInfoBlock.children[0].children[1].classList.remove('border-teal-500');
-            urlInfoBlock.children[0].children[1].classList.add('bg-red-100');
-            urlInfoBlock.children[0].children[1].classList.add('border-red-500');
-            urlInfoBlock.children[0].children[1].children[0].children[0].children[0].classList.remove('text-teal-500');
-            urlInfoBlock.children[0].children[1].children[0].children[0].children[0].classList.add('text-red-500');
+        catch (error) {
+            alert(error);
         }
-        urlInfoBlock.style.display = 'flex';
-        urlInfoBlock.children[0].children[1].children[0].children[1].children[0].textContent = heading;
-        urlInfoBlock.children[0].children[1].children[0].children[1].children[1].textContent = message;
+    });
+}
+function checkingServer(url) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const response = { exists: Math.random() >= 0.5, isFile: Math.random() >= 0.5 };
+            resolve(response);
+        }, 1000);
     });
 }
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ const urlInput:HTMLInputElement = document.getElementById('url') as HTMLInputEle
 const alertBox = document.getElementById("alert-box");
 const statusblock = document.getElementById("status");
 const urlInfoBlock = document.getElementById('url-info-card');
+const checkResult = document.getElementById("check-status")
+
 
 const PATTERN = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/;
 urlInput.addEventListener('input',
@@ -10,7 +12,7 @@ processURL)
 
 
 
-const thorottleURL = handleRequest(checkExistence,3000);
+const thorottleURL = debounceAndThrottle(checkExistence,300,1000);
 
 async function processURL(this:HTMLInputElement) {
     if(!URLChecker(this.value)){
@@ -21,7 +23,7 @@ async function processURL(this:HTMLInputElement) {
     }
     alertBox!.style.display = 'none';
    try {
-    await thorottleURL(this.value);
+    thorottleURL(this.value);
    } catch (error) {
     
    }
@@ -33,47 +35,49 @@ const URLChecker :(url:string)=> boolean =  (url) => {
      return PATTERN.test(url);
 }
 
-function handleRequest(func: Function, limit: number) {
-    let inThrottle = false;
-    return async function (url: string) {
-      const args = arguments;
-      const context = url;
-      if (!inThrottle) {
-        await func(context);
-        inThrottle = true;
-        setTimeout(() => (inThrottle = false), limit);
+
+  function debounceAndThrottle(func:any, delay:number, limit:number) {
+    let timerId:any;
+    let lastCallTime = 0;
+    let isThrottled = false;
+  
+    return (...args:any[]) => {
+      const currentTime = Date.now();
+      clearTimeout(timerId);
+  
+      if (currentTime - lastCallTime >= limit) {
+        func(args);
+        lastCallTime = currentTime;
+      } else {
+        timerId = setTimeout(() => {
+          func(args);
+          lastCallTime = currentTime;
+        }, delay);
       }
     };
   }
 
-async function checkExistence(url:string):Promise<void>{
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-    let message:string = '';
-    let heading:string = '';
-    const exists = Math.random() < 0.5; 
-    if(exists){
-         message = "The entered URL found on the server";
-         heading =  "Healthy URL";
-         urlInfoBlock!.children[0].children[1].classList.add('bg-teal-100')
-         urlInfoBlock!.children[0].children[1].classList.add('border-teal-500')
-         urlInfoBlock!.children[0].children[1].classList.remove('bg-red-100')
-         urlInfoBlock!.children[0].children[1].classList.remove('border-red-500')
-         urlInfoBlock!.children[0].children[1].children[0].children[0].children[0].classList.remove('text-red-500')
-         urlInfoBlock!.children[0].children[1].children[0].children[0].children[0].classList.add('text-teal-500')
-    }else{
-        message = "Sorry! the entred URL is not found";
-        heading =  "BAD URL";
-        urlInfoBlock!.children[0].children[1].classList.remove('bg-teal-100')
-        urlInfoBlock!.children[0].children[1].classList.remove('border-teal-500')
-        urlInfoBlock!.children[0].children[1].classList.add('bg-red-100')
-        urlInfoBlock!.children[0].children[1].classList.add('border-red-500')
-        urlInfoBlock!.children[0].children[1].children[0].children[0].children[0].classList.remove('text-teal-500')
-        urlInfoBlock!.children[0].children[1].children[0].children[0].children[0].classList.add('text-red-500')
-        
-    }
-    urlInfoBlock!.style.display='flex';
-    urlInfoBlock!.children[0].children[1].children[0].children[1].children[0].textContent = heading;
-    urlInfoBlock!.children[0].children[1].children[0].children[1].children[1].textContent = message;
-  
 
+  async function checkExistence(url:string) {
+    try {
+      const status:any = await checkingServer(url)
+      const result = status.exists ? (status.isFile ? 'File exists' : 'Folder exists') : 'URL does not exist';
+      urlInfoBlock!.style.display="flex";
+      checkResult!.textContent = result;
+    } catch (error) {
+      alert(error)
+    }
+   
   }
+
+
+  function checkingServer(url:string){
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const response = { exists: Math.random() >= 0.5, isFile: Math.random() >= 0.5 };
+        resolve(response);
+      }, 1000);
+    });
+  }
+
+
